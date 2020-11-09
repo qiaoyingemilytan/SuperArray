54df29dda553f7d0e903ea5de45501dcd0dbaf90
Emily <qtan20@stuy.edu>
2020-11-09 02:32:36 -0500

    changed

--- SuperArray.java
+++ SuperArray.java
@@ -124,4 +124,13 @@ public class SuperArray{
     return newArray;
   }
 
+  public int lastIndexOf(String value){
+    for(int i = size()-1; i >= 0; i--){
+      if(data[i].equals(value)){
+        return i;
+      }
+    }
+    return -1;
+  }
+
 }
